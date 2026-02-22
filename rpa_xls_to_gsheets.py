c5953493268d592e0b228de8881105121dedb73e
Davi Brandeburgo Hulse <davihulse@gmail.com>
2026-02-22 17:11:16 -0300

    Minor improvements

--- rpa_xls_to_gsheets.py
+++ rpa_xls_to_gsheets.py
@@ -25,7 +25,7 @@ from bs4 import BeautifulSoup
 #%%
 
 options = Options()
-#options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--disable-gpu")
 options.add_argument("--disable-notifications")
